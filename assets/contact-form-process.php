ae6c2bc8e675caefeba9b83e29ba8639eba343e5
BIKASH KUMAR MAHANTA <60123921+bikashkumarmahanta@users.noreply.github.com>
2020-07-17 17:34:56 +0530

    Update contact-form-process.php

--- assets/contact-form-process.php
+++ assets/contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "bkcreation2018@gmail.com";
+    $email_to = "bikash.mahanta2016@gmail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
